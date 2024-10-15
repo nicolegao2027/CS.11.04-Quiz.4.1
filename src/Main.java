00739d4e3777f8452591b07c50c3553b032ec5d8
Nicole Gao <gao.lieke@gmail.com>
2024-10-16 00:39:34 +0800

    IDE 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String str) {
+        int count = 0;
+        for (char ch : str.toLowerCase().toCharArray()) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -14,6 +23,17 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int notDivisibleBy235(int N) {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
@@ -24,6 +44,24 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
-
-
+    public static String camelCaseMe(String str) {
+        StringBuilder result = new StringBuilder();
+        boolean capitalizeNext = false;
+        for (char ch : str.toCharArray()) {
+            if (ch == '-' || ch == '_') {
+                capitalizeNext = true;
+            } else {
+                if (capitalizeNext) {
+                    result.append(Character.toUpperCase(ch));
+                    capitalizeNext = false;
+                } else {
+                    result.append(ch);
+                }
+            }
+        }
+        if (!str.isEmpty() && Character.isUpperCase(str.charAt(0))) {
+            result.setCharAt(0, Character.toUpperCase(result.charAt(0)));
+        }
+        return result.toString();
+    }
 }
